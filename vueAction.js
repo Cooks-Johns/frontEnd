d2700f7721a742900b093ccc0150d5ce4bac8b91
Cooks-Johns <cooks.johns@gmail.com>
2021-11-03 17:06:08 -0700

    Adding arrays to the data object and using the v-for to display them in a list

--- vueAction.js
+++ vueAction.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: ' If you want to change this in the js console use mountedApp.product just replace product with the object you want to manipulate. ',
             image: './assets/images/socks_green.jpg',
-            url: 'https://vuejs.org/v2/guide/#Getting-Started',
-            inventory: 100,
-            onSale: true
-            
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            // When you want to choose 
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ],
+            sizes: ['XS', 'S', 'M', 'L', 'XL']
+
         }
     }
 })
